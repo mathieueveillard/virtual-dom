c5a63736a5c7d28158a8604bad7f2e6bae87ef17
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2022-02-16 23:55:13 +0100

    Virtual DOM demo (2/3)

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(
-  React.createElement("h1", null, "An element of the Virtual DOM"),
+  <h1>An element of the Virtual DOM</h1>,
   document.getElementById("root")
 );
