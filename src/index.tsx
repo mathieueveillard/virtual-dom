95b840367c0e2296c178655f8247683da4759332
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2022-02-16 23:50:54 +0100

    Virtual DOM demo (1/3)

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  React.createElement("h1", null, "An element of the Virtual DOM"),
+  document.getElementById("root")
+);
